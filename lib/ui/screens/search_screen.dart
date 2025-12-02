cf9c5fe48718bce82bdd6f41a727c3283c7ae94a
DinOfficial <shdiinofficial19@gmail.com>
2025-12-03 02:27:39 +0300

    search page half ui complete

--- lib/ui/screens/search_screen.dart
+++ lib/ui/screens/search_screen.dart
@@ -12,6 +12,103 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(child: Text('Search Page'));
+    return SizedBox(
+      child: Padding(
+        padding: EdgeInsets.all(12),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.search_sharp),
+                filled: true,
+                // fillColor: Colors.grey.shade200,
+                hintText: 'Search by keywords',
+                hintStyle: TextStyle(color: Colors.black38),
+                focusColor: Colors.grey.shade800,
+              ),
+            ),
+            const SizedBox(height: 12),
+            Divider(),
+            const SizedBox(height: 12),
+            const Text('Popular Search', style: TextStyle(fontSize: 16, color: Colors.redAccent, fontWeight: FontWeight.bold),),
+            const SizedBox(height: 12,),
+            Wrap(
+              alignment: WrapAlignment.start,
+              spacing: 20,
+              runSpacing: 4,
+              children: [
+                TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1, color: Colors.black26),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('Ukrain'),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1, color: Colors.black26),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('Suprem Court'),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1, color: Colors.black26),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('China'),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1, color: Colors.black26),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('Inflaction'),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1, color: Colors.black26),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('Oil Price'),
+                ),
+                TextButton(
+                  style: TextButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      side: BorderSide(width: 1, color: Colors.black26),
+                      borderRadius: BorderRadius.circular(8),
+                    ),
+                  ),
+                  onPressed: () {},
+                  child: Text('Plan Crash'),
+                ),
+              ],
+            ),
+            const SizedBox(height: 12,),
+            Divider(),
+            const SizedBox(height: 12,),
+            const Text('Popular Articles', style: TextStyle(fontSize: 16, color: Colors.redAccent, fontWeight: FontWeight.bold),),
+            const SizedBox(height: 12,),
+          ],
+        ),
+      ),
+    );
   }
 }
